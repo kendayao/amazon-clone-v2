a612b545cb78817758cf1bf242075bec9b33588c
Kaeneth Dayao <kendayao@gmail.com>
2025-01-09 19:16:31 -0800

    created and styled home banner

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className="home">
+        <div className="home__container">
+            <img className="home__image" alt="amazon banner ad" src="https://m.media-amazon.com/images/I/81mXStzsdmL._SX3000_.jpg" />
+        </div>
+    </div>
+  )
+}
+
+export default Home
