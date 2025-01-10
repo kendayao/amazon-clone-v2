384fd0fba72271669c1acdb3dd6c87f91e667224
Kaeneth Dayao <kendayao@gmail.com>
2025-01-09 20:21:45 -0800

    styled product

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Home.css'
+import Product from"./Product"
 
 function Home() {
   return (
     <div className="home">
         <div className="home__container">
             <img className="home__image" alt="amazon banner ad" src="https://m.media-amazon.com/images/I/81mXStzsdmL._SX3000_.jpg" />
+            <div className="home__row">
+                <Product/>
+            </div>
         </div>
     </div>
   )
