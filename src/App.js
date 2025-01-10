65a25f164ae8e6d6ccd914e7c9b1b52e08d7052c
Kaeneth Dayao <kendayao@gmail.com>
2025-01-09 18:10:53 -0800

    header options created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
 import './App.css';
+import Header from "./components/Header"
 
 function App() {
   return (
-  
-  <h1>test</h1>
+    <Header />
 
 )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,23 @@ import './Header.css'
 function Header() {
   return (
     <div className='header'>
-        <img className="header__logo" alt="amazon logo" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Famazon-logo-png-free-download-in-2023--952370652432471077%2F&psig=AOvVaw0Msd13jDCuciuGoRXHSj7E&ust=1736560339359000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMi0mMeF6ooDFQAAAAAdAAAAABAE"/>
+        <img className="header__logo" alt="amazon logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
         <div className="header__search">
             <input className="header__searchInput" type="text" />
         </div>
         <div className="header__nav">
-
+            <div className="header__option">
+                <span className="header__optionLineOne">Hello Guest</span>
+                <span className="header__optionLineTwo">Sign In</span>
+            </div>
+            <div className="header__option">
+                <span className="header__optionLineOne">Returns</span>
+                <span className="header__optionLineTwo">& Orders</span>
+            </div>
+            <div className="header__option">
+                <span className="header__optionLineOne">Your</span>
+                <span className="header__optionLineTwo">Prime</span>
+            </div>
         </div>
         
 
