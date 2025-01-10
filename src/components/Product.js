67fed9c34cc55a08786ce49268619f6f26c03809
Kaeneth Dayao <kendayao@gmail.com>
2025-01-10 07:45:22 -0800

    styled products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import "./Product.css"
 
-function Product() {
+function Product({title, price, image, rating}) {
   return (
     <div className= "product">
         <div className="product__info">
-            <p>The lean startup</p>
+            <p>{title}</p>
             <p className="product__price">
                 <small>$</small>
-                <strong>19.99</strong>
+                <strong>{price}</strong>
             </p>
             <div className="product__rating">
-                <p>⭐</p>
+            {Array(rating).fill().map((_,i)=>(<p>⭐</p>))}
+                
             </div>
         </div>
-        <img src="https://images-na.ssl-images-amazon.com/images/I/61Rxx9IF88L._AC_SX522_.jpg"/>
+        <img src={image}/>
         <button>Add to Basket</button>
     </div>
   )
