68217bcb299b29a588b3aba67170d897d9794314
Kaeneth Dayao <kendayao@gmail.com>
2025-01-10 23:13:31 -0800

    baskettotal selector

--- src/components/StateProvider.js
+++ src/components/StateProvider.js
@@ -1,4 +1,10 @@
 import React, {createContext, useContext, useReducer} from "react";
+//setup context api
+    //1) create state provider file (this file)
+    //2) Wrap state provider to index.js file
+    //3) create reducer file
+
+
 
 // Prepares the data layer
 export const StateContext=createContext();
