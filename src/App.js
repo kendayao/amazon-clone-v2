f197cc0480887afec80eab020f838f51aaf69821
Kaeneth Dayao <kendayao@gmail.com>
2025-01-10 17:14:57 -0800

    set up rotuer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Header from "./components/Header"
 import Home from"./components/Home"
+import { BrowserRouter as Routerr, Switch, Route } from "react-router-dom"
 
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
+    <Routes>
+      <div className="app">
+        <Route path="/" element={<Header/>}>
+            <Route index element={<Home/>} />
+            <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </div>
+    </Routes>
+   
 )
 }
 
